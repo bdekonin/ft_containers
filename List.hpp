b40de286f4b1fc6f8264da07df604cceee642c73
bdekonin <bdekonin@student.codam.nl>
2021-03-04 19:11:31 +0100

    .

--- List.hpp
+++ List.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   list.hpp                                           :+:    :+:            */
+/*   List.hpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/22 13:03:26 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/03/01 21:57:15 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/03/04 19:03:19 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 #include <iostream>
 
-
-
 template <typename T>
 class List;
 
@@ -24,7 +22,7 @@ template <typename T>
 class ListIterator
 {
 	public:
-		// test
+		typedef T &reference;
 		ListIterator()
 		{
 		}
@@ -39,6 +37,18 @@ class ListIterator
 		ListIterator(_node *node) : _n(node) // only accessed by friend classes
 		{
 		}
+
+		/* Methods */
+		_node *getHead(_node *node)
+		{
+			while (node->prev)
+			{
+				node = node->prev;
+				std::cout << "i";
+			}
+			std::cout << "\n";
+			return (node);
+		}
 	public:
 
 		ListIterator<T>  operator=(const ListIterator<T>  &o)
@@ -48,7 +58,10 @@ class ListIterator
 		}
 		ListIterator<T>  operator++(int)
 		{
-			this->_n = this->_n->next;
+			if (this->_n->next == nullptr)
+				this->_n = this->getHead(this->_n);
+			else
+				this->_n = this->_n->next;
 			return *this;
 		}
 		ListIterator<T>  operator--(int)
@@ -56,6 +69,11 @@ class ListIterator
 			this->_n = this->_n->prev;
 			return *this;
 		}
+		reference operator*() const
+		{
+			return this->_n->content;
+		}
+
 
 
 		// So List can call a private constructor
@@ -107,42 +125,74 @@ class List
 		{
 			this->_head = new _node(nullptr, nullptr, val);
 			this->_tail = this->_head;
-			this->insert(this->begin(), n, val);
+			if (n > 1)
+				this->insert(this->begin(), n - 1, val);
 		}
-
+		
 		/* Methods */
 		void insert(iterator position, size_type n, value_type val)
 		{
-			_node *alst = new _node(nullptr, nullptr, val);
-			_node *last = alst;
-			_node *newNode;
-			for (size_type i = 0; i < (n - 1); i++)
+			_node *newHead = new _node(nullptr, nullptr, val);
+			_node *lastNewHead;
+			for (size_type i = 1; i < n; i++)
+				this->ft_lstadd_back(&newHead, new _node(nullptr, nullptr, val));
+			lastNewHead = this->ft_lstlast(newHead);
+			
+			if (position._n->prev == nullptr)
 			{
-				newNode = new _node(nullptr, nullptr, val);
-				last->next = newNode;
-				newNode->prev = last;
+				position._n->prev = lastNewHead;
+				lastNewHead->next = position._n;
+				this->_head = newHead;
 			}
-			_node *positionNode = position._n;
-			if (positionNode->prev)
+			else if (position._n == this->ft_lstlast(this->_head))
 			{
-				positionNode->prev->next = 
+				this->ft_lstadd_back(&this->_head, newHead);
+			}
+			else
+			{
+				_node *left = position._n->prev;
+				
+				left->next = newHead;
+				newHead->prev = left;
+				
+				position._n->prev = lastNewHead;
+				lastNewHead->next = position._n;
 			}
 		}
 
-
-				/* Methods */
-				void insert()
-				{
-					this->_head->next = new _node(this->_head, nullptr, 1);
-					this->_head->next->next = new _node(this->_head->next, nullptr, 2);
-					this->_head->next->next->next = new _node(this->_head->next->next, nullptr, 3);
-					this->_head->next->next->next->next = new _node(this->_head->next->next->next, nullptr, 4);
-				}
+		void print()
+		{
+			for (_node *test = this->_head; test; test = test->next)
+				std::cout << test->content << std::endl;
+		}
 		iterator begin()
 		{
 			return iterator(this->_head);
 		}
 	private:
+		void ft_lstadd_back(_node **head, _node *neww)
+		{
+			_node *last;
+
+			if (!neww)
+				return ;
+			if (*head)
+			{
+				last = ft_lstlast(*head);
+				last->next = neww;
+				neww->prev = last;
+			}
+		}
+		_node *ft_lstlast(_node *lst)
+		{
+			if (!lst)
+				return nullptr;
+			while (lst->next != NULL)
+				lst = lst->next;
+			this->_tail = lst;
+			return (lst);
+		}
+		
 		template <typename _T>
 		friend class ListIterator; // So ListIterator can access private rights
 };
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/22 13:48:17 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/03/01 21:56:29 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/03/04 18:47:45 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,48 @@
 #include <list> 
 #include <iterator>
 
+::List<int>		list1 (3, 100);
+std::list<int>	list3 (3, 100);
+
+::List<int>::iterator it1 = list1.begin();
+std::list<int>::iterator it3 = list3.begin();
+
+void print()
+{
+	::List<int>::iterator it2 = list1.begin();
+	std::list<int>::iterator it6 = list3.begin();
+
+	it2++;
+	it6++;
+	it2++;
+	it6++;
+	it2++;
+	it6++;
+	it2++;
+	it6++;
+
+	*it2 = 21;
+		*it6 = 21;
+
+	std::cout << "--> "<< *it2 << " " << *it6 << "\n";
+	
+
+	// list1.insert(it2, 3, 20);
+	// list3.insert(it6, 3, 20);
+
+	it1 = list1.begin();
+	it3 = list3.begin();
+	while (it3 != list3.end())
+	{
+		std::cout << *it1 << " " << *it3 << "\n";
+
+		it1++;
+		it3++;
+	}
+	
+}
 
 int main(void)
 {
-	::List<int>::iterator it;
-	::List<int> list (4, 100);
-
-	it = list.begin();
-	it.print();
-	it++;
-	it.print();
+	print();
 }
-
-// NULL == nullptr
