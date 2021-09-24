ca8441d71dd1782ccb9345ddb9f6496fe7f8d5d6
Bob Dekonink <bdekonin@f0r6s1.codam.nl>
2021-09-24 11:32:44 +0200

    confusion

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 14:25:26 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/09/23 20:59:19 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/09/24 11:31:04 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,65 @@
 
 int main(void)
 {
-	Vector<int> *kaas = new Vector<int>();
 
-	// kaas->print(10);
+	{
+		std::cout << "std::vector:\t";
+		std::vector<int> myvector;
+		for (int i = 1; i < 10; i++)
+			myvector.push_back(i);		
+		myvector.resize(3);
+		myvector.resize(8);
+		// myvector.resize(12);
+		std::cout << "cap: (" << myvector.capacity() << ")\t:";
+		std::cout << "size: (" << myvector.size() << ")\t:";
+		for (int i=0;i<myvector.size();i++)
+			std::cout << ' ' << myvector[i];
+		std::cout << '\n';
+	}
+	{
+		std::cout << "My::Vector:\t";
+		Vector<int> myvector;
+		
+		for (int i = 1; i < 10; i++)
+			myvector.push_back(i);
 
-	kaas->reserve(50);
+			std::cout << "cap: (" << myvector.capacity() << ")\t:";
+			std::cout << "size: (" << myvector.size() << ")\t:";
+			myvector.print(myvector.size());
 
-	kaas->fill(25);
+		myvector.resize(3);
+			std::cout << "cap: (" << myvector.capacity() << ")\t:";
+			std::cout << "size: (" << myvector.size() << ")\t:";
+			myvector.print(myvector.size());
+		
+		myvector.resize(8);
+			std::cout << "cap: (" << myvector.capacity() << ")\t:";
+			std::cout << "size: (" << myvector.size() << ")\t:";
+			myvector.print(myvector.size());
+		
+		// myvector.resize(12);
+			std::cout << "cap: (" << myvector.capacity() << ")\t:";
+			std::cout << "size: (" << myvector.size() << ")\t:";
+			myvector.print(myvector.size());
+		
+		std::cout << "cap: (" << myvector.capacity() << ")\t:";
+		std::cout << "size: (" << myvector.size() << ")\t:";
+		myvector.print(myvector.size());
+		std::cout << "\n";
+	}
+
+	
+
+
+	// myvector.print(myvector.size());
+	// std::cout << "\n\n";
+	
+	// myvector.print(myvector.size());
+	// std::cout << "\n\n";
+	
+	// myvector.print(myvector.size() + 1);
+	// std::cout << "\n\n";
+	// std::cout << myvector.size() << std::endl;
+	// std::cout << myvector.capacity() << std::endl;
 
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 11:53:29 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/09/23 20:58:26 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/09/24 11:30:22 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,41 +42,127 @@ class Vector
 	: _begin(nullptr), _end(nullptr), _capacity(nullptr), _alloc(alloc)
 	{
 	};
-	
-	size_type	capacity(void) const
+	Vector (size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
+	: _begin(nullptr), _end(nullptr), _capacity(nullptr), _alloc(alloc)
 	{
-		return _capacity - _begin;
-	}
-	size_type	size(void) const
-	{
-		return _end - _begin;
-	}
-	/*
-	** https://www.cplusplus.com/reference/vector/vector/reserve/
-	*/
-	void reserve (size_type n) /* Requests that the vector capacity be at least enough to contain n elements. */
-	{
-		pointer oldStart = this->_begin;
-		pointer oldEnd = this->_end;
-		size_type oldN = this->size();
-		size_type oldCap = this->capacity();
-
 		this->_begin = this->_alloc.allocate(n);
 		this->_end = this->_begin;
 		this->_capacity = this->_begin + n;
 
-		while (oldStart != oldEnd)
-			this->_alloc.construct(this->_end++, *oldStart++);
-		this->_alloc.deallocate(oldStart - oldN, oldCap);
+		for (size_type i = 0; i < n; i++)
+		{
+			this->_begin[i] = val;
+		}
 	}
 
-	void fill(size_type n)
-	{
-		for (size_type i = 0; i < n; i++)
-			this->_begin[i] = i;
-	}
+
+	/* Capacity */
+		/* Return size. */
+		size_type size() const
+		{
+			return this->_end - this->_begin;
+		}
+		/* Return maximum size */
+		size_type max_size() const
+		{
+			return this->_alloc.max_size();
+		}
+		/* Resizes the container so that it contains n elements. */
+		void resize(size_type n, value_type val = value_type())
+		{
+			pointer oldStart = this->_begin;
+			pointer oldEnd = this->_end;
+			size_type oldSize = this->size();
+			size_type oldCap = this->capacity();
+
+			// if (n <= this->capacity())
+			// {
+			// 	std::cout << "n is <= then the capacity" << std::endl;
+			// 	return ;
+			// }
+			if (n < this->size())
+			{
+				while (this->size() > n)
+				{
+					--this->_end;
+					this->_alloc.destroy(this->_end);
+				}
+			}
+			else
+			{
+				// std::cout << "ja - " << n << "[" << val << "]\n";
+				this->_begin = this->_alloc.allocate(n);
+				this->_end = this->_begin + oldSize;
+				this->_capacity = this->_begin + n;
+
+				// printf("-----%d", n);
+
+				for (size_type i = 0; i < this->size(); i++)
+					this->_begin[i] = val; // fill entire thing
+				for (size_type i = 0; i < oldCap; i++)
+					this->_begin[i] = *(oldStart + i);
+				this->_alloc.deallocate(oldStart, oldCap);
+			}
+		}
+
+
+			// void        resize (size_type n, value_type val = value_type())
+			// {
+			// 	if (n > this->max_size())
+			// 		throw (std::length_error("vector::resize"));
+			// 	else if (n < this->size())
+			// 	{
+			// 		while (this->size() > n)
+			// 		{
+			// 			--_end;
+			// 			_alloc.destroy(_end);
+			// 		}
+			// 	}
+			// 	else
+			// 		this->insert(this->end(), n - this->size(), val);
+			// }
+
+
+			
+		/*Return ize of allocated storage capacity */
+		size_type capacity() const
+		{
+			return this->_capacity - this->_begin;
+		}
+
+
+		/* temporary */
+			void print(size_type const n) const
+			{
+				for (size_type i = 0; i < n; i++)
+				{
+					std::cout << ' ' << this->_begin[i];
+				}
+				std::cout  << std::endl;
+			}
+			void push_back (const value_type &val)
+			{
+				if (this->_end == this->_capacity)
+				{
+					// int next_capacity = (this->size() > 0) ? (int)(this->size() * 2) : 1;
+					this->resize(this->capacity() + 1);
+				}
+				this->_alloc.construct(this->_end, val);
+				this->_end++;
+			}
+			// void push_back (const value_type& val)
+			// {                
+			// 	if (_end == _end_capacity)
+			// 	{
+			// 		int next_capacity = (this->size() > 0) ? (int)(this->size() * 2) : 1;
+			// 		this->reserve(next_capacity);
+			// 	}
+			// 	_alloc.construct(_end, val);
+			// 	_end++;
+			// }
 	private:
 		// pointer	_listarray; /* https://en.wikipedia.org/wiki/Dynamic_array */
+		
 		allocator_type	_alloc;
 		pointer			_begin;
 		pointer			_end;
